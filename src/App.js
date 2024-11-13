e8f0f222e583c11672e0b0c539ee9a8629080be3
Yash <shuklayash1304@gmail.com>
2024-11-13 14:20:35 +0530

    Calculator Implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const arr = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "+",
+    "-",
+    "/",
+    "*",
+    "=",
+    "C",
+    ".",
+  ];
+
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    const id = e.target.id;
+    if (id === "C") {
+      setValue("");
+    } else if (id === "=") {
+      // Result
+      handleSubmit();
+    } else {
+      setValue((val) => val + id);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e?.preventDefault();
+    try {
+      const ans = eval(value);
+      setValue(ans);
+    } catch (err) {
+      alert("Invalid Inputs");
+    }
+  };
+  // console.log(value);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={value} onChange={handleChange} type="text" />
+      </form>
+      <div className="container" onClick={handleClick}>
+        {arr.map((item, i) => (
+          <button id={item} key={i} className="cell">
+            {item}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
